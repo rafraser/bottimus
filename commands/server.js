bcdc87e5d657a94e676b3964a53f9fb1c37c92b8
FluffyXVI <fluffyxvi@gmail.com>
2019-02-08 23:36:18 +1000

    Server query and stats commands

---
+++ commands/server.js
@@ -0,0 +1,24 @@
+const discord = require('discord.js')
+const gamedig = require('gamedig')
+const ip = '108.61.169.175'
+
+module.exports = {
+    name: 'status',
+    description: 'Fetchs current server state from Simply Murder',
+    execute(message, args) {
+        // Query the server using gamedig
+        gamedig.query({type:'garrysmod', host:ip}).then(function(result) {
+            // Generate a nice looking embed
+            var embed = new discord.RichEmbed()
+            .setColor(43263)
+            .setTitle(`${result.name}`)
+            .setDescription(`Click: steam://connect/${ip} to join`)
+            .addField('Players', `${result.players.length||0}/${result.maxplayers||0}`, true)
+            .addField('Map', `${result.map}`, true)
+            .setThumbnail(`https://fluffyservers.com/mapicons/${result.map}.jpg`)
+            message.channel.send(embed)
+        }).catch(function(error) {
+            message.channel.send('Something went wrong. Is the server down?')
+        })
+    },
+};
---
+++ commands/stats.js
@@ -0,0 +1,49 @@
+const db = require('../database')
+const discord = require('discord.js')
+
+module.exports = {
+    name: 'stats',
+    description: 'Fetchs statistics from Simply Murder',
+    execute(message, args) {
+        if(!args.length || args.length < 1) { return }
+        
+        // Determine argument type
+        var id = args[0]
+        var query_type = 'username'
+        if (/^-{0,1}\d+$/.test(id) && id.startsWith('7')) { query_type = 'steamid64' }
+        
+        // Pick query based on argument given
+        var query_string
+        if (query_type == 'username') {
+            query_string = 'SELECT * FROM playtime p LEFT JOIN stats_murder m ON p.steamid64 = m.steamid64 LEFT JOIN event_cake e ON p.steamid64 = e.steamid64 LEFT JOIN location l ON p.steamid64 = l.steamid64 WHERE p.username=? LIMIT 1'
+        } else {
+            query_string = 'SELECT * FROM playtime p LEFT JOIN stats_murder m ON p.steamid64 = m.steamid64 LEFT JOIN event_cake e ON p.steamid64 = e.steamid64 LEFT JOIN location l ON p.steamid64 = l.steamid64 WHERE p.steamid64=? LIMIT 1'
+        }
+        
+        // Query database
+        db(function(err, conn) {
+            conn.query(query_string, [id], function(err, results) {
+                if (err) {
+                    message.channel.send('Couldn\'t get stats :(')
+                } else {
+                    // results! yay!
+                    try {
+                        var result = results[0]
+                        // Generate a fancy looking embed with the user statistics
+                        var embed = new discord.RichEmbed()
+                            .setColor('')
+                            .setTitle(`Stats for ${result.username}`)
+                            .addField('Hours Played', `${Math.floor(result.playtime/3600)||0}`, true)
+                            .addField('Loot Collected', `${result.loot||0}`, true)
+                            .addField('Murders', `${result.murders||0}`, true)
+                            .addField('Shot Murderers', `${result.shot_murderer||0}`, true)
+                            .addField('Shot Innocents', `${result.shot_innocent||0}`, true)
+                            .addField('Shooting %', `${Math.floor(result.shot_murderer/(result.shot_murderer+result.shot_innocent)*100) || 0}%`, true)
+                            .addField('Cakes Collected', `${result.cake_total||0}`, true)
+                        message.channel.send(embed)
+                    } catch (e) { message.channel.send('Couldn\'t get stats :(') }
+                }
+            })
+        })
+    },
+}
