c7b56c2e650b3b02ceb5a93d1cafdb455e0f338c
FluffyXVI <fluffyxvi@gmail.com>
2019-02-08 23:31:11 +1000

    8ball command + images
    Simple command that sends one of twenty random images as a reply

---
+++ commands/8ball.js
@@ -0,0 +1,17 @@
+const discord = require('discord.js')
+const path = require('path')
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+module.exports = {
+    name: '8ball',
+    description: 'Ask the magic 8ball a question... if you dare',
+    execute(message, args) {
+        // Pick one of the 20 8ball images at random and send it as a reply
+        var result = getRandomInt(1, 20)
+        var img = new discord.Attachment(path.join(__dirname, '../img/8ball/' + result + '.png'))
+        message.channel.send(img)
+    },
+};
