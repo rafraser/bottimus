9e93c06911a55d4eebc02ddc310774912a9636fe
FluffyXVI <fluffyxvi@gmail.com>
2019-02-09 12:24:27 +1000

    Add !help to usefully print a list of command
    Uses code formatting to make things look extra nice

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'help',
+    description: 'List all the commands the bot can use',
+    execute(message, args, client) {
+        var codestring = 'List of commands:\n```python\n'
+        for(cmd of client.commands) {
+            codestring += cmd[0].padEnd(10, ' ') + ' "' + cmd[1].description + '"\n'
+        }
+        codestring += '```'
+        message.channel.send(codestring)
+    },
+};
