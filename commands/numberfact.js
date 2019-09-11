2ce557a3141173e3f9766506937c693c2e182203
FluffyXVI <fluffyxvi@gmail.com>
2019-09-11 22:03:36 +1000

    Implemented !numberfact

---
+++ commands/numberfact.js
@@ -0,0 +1,37 @@
+const discord = require('discord.js');
+const http = require('http')
+
+module.exports = {
+    name: 'numberfact',
+    description: 'Get a random number fact',
+    execute(message, args) {
+        // Specifically this fetchs a random image tagged meme off flickr
+        // It's extremely unpredictable at best, hence the wacky nature!
+        var url
+        if(Math.random() < 0.2) {
+            url = 'http://numbersapi.com/random/trivia'
+        } else {
+            url = 'http://numbersapi.com/random/math'
+        }
+        
+        http.get(url, function(resp) {
+            data = '';
+            
+            resp.on('data', function(chunk) {
+                data += chunk;
+            })
+            
+            resp.on('end', function() {
+                data = data.split(' ')
+                var number = data.shift()
+                var description = data.join(' ')
+                
+                var embed = new discord.RichEmbed()
+                .setColor('#9c88ff')
+                .setTitle(number)
+                .setDescription(description)
+                message.channel.send(embed)
+            })
+        })
+    },
+};
