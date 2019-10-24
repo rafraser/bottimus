78e6f4b6b91e81450c71ebef845b57a8d72519e8
Robert Fraser <robert.fraser@uqconnect.edu.au>
2019-10-24 18:45:50 +1000

    Testing command for spinners

---
+++ commands/spin.js
@@ -0,0 +1,25 @@
+const arcade = require('../arcade')
+const pool = require('../database')
+const discord = require('discord.js')
+
+module.exports = {
+    name: 'spintest',
+    description: 'Spin the test wheel',
+    execute(message, args, client) {
+        if(client.spinningWheel) return
+        
+        client.spinningWheel = true
+        
+        message.channel.send('Building the wheel...').then(function(m) {
+            client.executePython('spinner', args).then(function(data) {
+                var attachment = new discord.Attachment('./img/spinner.gif')
+                message.channel.send(attachment).then(function() {
+                    setTimeout(function() { 
+                        message.channel.send(data)
+                        client.spinningWheel = null
+                    }, 4000)
+                })
+            })
+        })
+    },
+}
