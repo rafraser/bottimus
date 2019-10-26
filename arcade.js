c1175a9ebd4ebb562f33137c847f9476ea2b3782
Robert Fraser <robert.fraser@uqconnect.edu.au>
2019-10-27 01:50:48 +1000

    Arcade library now has prize helper functions

--- arcade.js
+++ arcade.js
@@ -11,12 +11,50 @@ function incrementArcadeCredits(userid, amount) {
 
 function getArcadeCredits(userid) {
     var p = new Promise(function(resolve, reject) {
-    var query_string = "SELECT * FROM arcade_currency WHERE userid = ?"
+        var query_string = "SELECT * FROM arcade_currency WHERE userid = ?;"
         pool.query(query_string, [userid], function(err, results) {
             if(err) {
                 reject(err)
             } else {
+                if(results.length < 1) resolve(0)
                 resolve(results[0].amount || 0)
+            }
+        })
+    })
+    
+    return p
+}
+
+function unlockArcadePrize(userid, prize) {
+    if(!prizeList[prize]) return false
+    
+    var p = new Promise(function(resolve, reject) {
+        var query_string = "INSERT INTO arcade_prizes VALUES(?, ?, 1) ON DUPLICATE KEY UPDATE amount = amount + 1;"
+        pool.query(query_string, [userid, prize], function(err, results) {
+            if(err) {
+                console.log(err)
+            }
+        })
+    })
+    
+    return p
+}
+
+function getArcadePrizes(userid) {
+    var p = new Promise(function(resolve, reject) {
+        var query_string = "SELECT * FROM arcade_prizes WHERE discordid = ?;"
+        pool.query(query_string, [userid], function(err, results) {
+            if(err) {
+                reject(err)
+            } else {
+                var prizes = {}
+                for(var i=0; i<results.length; i++) {
+                    var name = results[i]['prize']
+                    var amount = results[i]['amount']
+                    prizes[name] = amount
+                }
+                
+                resolve(prizes)
             }
         })
     })
@@ -28,3 +66,7 @@ module.exports.incrementArcadeCredits = incrementArcadeCredits
 module.exports.getArcadeCredits = getArcadeCredits
 module.exports.incrementCredits = incrementArcadeCredits
 module.exports.getCredits = getArcadeCredits
+module.exports.getArcadePrizes = getArcadePrizes
+module.exports.unlockArcadePrize = unlockArcadePrize
+module.exports.prizes = prizeList
+module.exports.rarities = prizeRarities
