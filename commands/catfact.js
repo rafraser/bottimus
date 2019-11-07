d16c23eb56ec7550091ef923039854a683254eec
Robert Fraser <robert.fraser@uqconnect.edu.au>
2019-11-08 02:17:12 +1000

    Bring back fun fact commands

---
+++ commands/catfact.js
@@ -0,0 +1,27 @@
+const discord = require('discord.js');
+const https = require('https')
+
+module.exports = {
+    name: 'catfact',
+    description: 'Get a random cat fact',
+    execute(message, args) {
+        var url = 'https://catfact.ninja/fact'
+        
+        https.get(url, function(resp) {
+            data = '';
+            
+            resp.on('data', function(chunk) {
+                data += chunk;
+            })
+            
+            resp.on('end', function() {
+                var fact = JSON.parse(data)['fact']
+                
+                var embed = new discord.RichEmbed()
+                .setColor('#9c88ff')
+                .setDescription(fact)
+                message.channel.send(embed)
+            })
+        })
+    },
+};
