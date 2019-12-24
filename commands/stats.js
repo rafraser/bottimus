fad95e6defd541930d91454c14911fd5ff0fab9c
Robert Fraser <robert.fraser@uqconnect.edu.au>
2019-12-25 01:15:48 +1000

    Finally fix !stats

--- commands/stats.js
+++ commands/stats.js
@@ -1,5 +1,6 @@
 const pool = require('../database')
 const discord = require('discord.js')
+const https = require('https')
 
 module.exports = {
     name: 'stats',
@@ -13,29 +14,18 @@ module.exports = {
             args[0] = args.join(" ")
         }
         
-        // Determine argument type
-        var id = args[0]
-        var query_type = 'username'
-        if (/^-{0,1}\d+$/.test(id) && id.startsWith('7')) { query_type = 'steamid64' }
-        
-        // Pick query based on argument given
-        var query_string
-        if (query_type == 'username') {
-            query_string = 'SELECT * FROM playtime p WHERE p.username LIKE ? ORDER BY playtime DESC LIMIT 1;'
-            id = '%' + id + '%'
-        } else {
-            query_string = 'SELECT * FROM playtime p WHERE p.steamid64=? LIMIT 1;'
-        }
-        
-        // Query database
-        var q = pool.query(query_string, [id], function(err, results) {
-            if (err) {
-                message.channel.send('Couldn\'t get stats :(')
-                message.channel.send(err.toString())
-            } else {
-                // results! yay!
+        // Query Fluffy Servers API
+        var url = 'https://fluffyservers.com/api/stats/search/' + args[0]
+        https.get(url, function(resp) {
+            data = '';
+            
+            resp.on('data', function(chunk) {
+                data += chunk;
+            })
+            
+            resp.on('end', function() {
                 try {
-                    var result = results[0]
+                    var result = JSON.parse(data)['results'][0]
                     // Generate a fancy looking embed with the user statistics
                     var embed = new discord.RichEmbed()
                         .setColor('#e84118')
@@ -46,7 +36,7 @@ module.exports = {
                 } catch (e) {
                     message.channel.send('User not found in database')
                 }
-            }
+            })
         })
     },
 }
