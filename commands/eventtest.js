abb3114a002aaffa2ca92ff8fbe9fc5e8c8c626c
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2019-12-27 16:09:14 +1000

    Date & time handling when creating events

--- commands/eventtest.js
+++ commands/eventtest.js
@@ -17,8 +17,50 @@ module.exports = {
             client.eventsData = new discord.Collection()
         }
 
+        try {
+            var title = args.shift()
+            var description = args.shift()
+            
+            var now = new Date(Date.now())
+            var datetime = {
+                year: now.getFullYear(),
+                month: now.getMonth(),
+                day: now.getDate()
+            }
+
+            while(args.length >= 1) {
+                var arg = args.shift()
+                if(arg.includes(':')) {
+                    // Try parsing this argument as time
+                    arg = arg.split(':')
+                    datetime.hour = arg[0]
+                    datetime.minute = arg[1]
+                } else if(arg.includes('-')) {
+                    // Try parsing this argument as YYYY-MM-DD
+                    arg = arg.split('-')
+                    datetime.year = arg[0]
+                    datetime.month = arg[1]
+                    datetime.day = arg[2]
+                } else if(arg.includes('/')) {
+                    // Try parsing this argument as DD/MM/YYYY
+                    arg = arg.split('/')
+                    datetime.day = arg[0]
+                    datetime.month = arg[1]
+                    datetime.year = arg[2]
+                }
+            }
+
+            console.log(title, description, datetime)
+            
+            var when = new Date(datetime.year, datetime.month, datetime.day, datetime.hour, datetime.minute)
+            console.log(when)
+        } catch(e) {
+            console.error(e)
+            message.channel.send('Invalid event structure')
+        }
+
         // Generate an initial event embed
-        var event = events.generateEvent(message.member, args[0], args[1], new Date(Date.now() + 1000*15))
+        var event = events.generateEvent(message.member, title, description, when)
         var timeLeft = client.timeToString(event.time - Date.now())
         var embed = events.generateEventEmbed(event, timeLeft)
 
