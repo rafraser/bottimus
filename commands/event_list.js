949cf0f32b0d0c01b9d404eb1fa3d9844dd68201
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-01-21 02:19:25 +1000

    Event listing command

---
+++ commands/event_list.js
@@ -0,0 +1,28 @@
+module.exports = {
+  name: 'eventlist',
+  description: 'Get a brief list of what events are upcoming',
+  aliases: ['events'],
+  cooldown: 10,
+  execute (message, args, client) {
+    if (!client.eventsData || client.eventsData.size < 1) {
+      message.channel.send('No events are currently scheduled!')
+      return
+    }
+
+    console.log(client.eventsData.size)
+
+    // Sort the events by whichever is soonest
+    const sortedEvents = client.eventsData.sort(function (a, b) {
+      return a.time - b.time
+    }).array()
+
+    // Generate a code block list
+    let outputString = '```cs\n# Upcoming Events #'
+    sortedEvents.forEach(function (item, index) {
+      console.log(item.time, index)
+      outputString += '\n ' + (index + 1) + '. ' + item.title
+    })
+    outputString += '```'
+    message.channel.send(outputString)
+  }
+}
