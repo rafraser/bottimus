521569282833631e6de585030d22137ca42b1efb
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-02-27 16:23:00 +1000

    Dailyspin tracks user data

--- commands/dailyspin.js
+++ commands/dailyspin.js
@@ -4,7 +4,7 @@ const discord = require('discord.js')
 
 function getLastSpin(id) {
   return new Promise(function (resolve, reject) {
-    var queryString = 'SELECT lastspin FROM arcade_dailyspin WHERE discordid = ?'
+    const queryString = 'SELECT lastspin FROM arcade_dailyspin WHERE discordid = ?'
     pool.query(queryString, [id], function (err, results) {
       if (err) {
         reject(err)
@@ -16,8 +16,13 @@ function getLastSpin(id) {
 }
 
 function updateLastSpin(id, date) {
-  var queryString = 'INSERT INTO arcade_dailyspin VALUES(?, ?, 1) ON DUPLICATE KEY UPDATE lastspin = VALUES(lastspin), number = number + VALUES(number)'
+  const queryString = 'INSERT INTO arcade_dailyspin VALUES(?, ?, 1) ON DUPLICATE KEY UPDATE lastspin = VALUES(lastspin), number = number + VALUES(number)'
   pool.query(queryString, [id, date])
+}
+
+function updateUserData(user) {
+  const queryString = 'INSERT INTO bottimus_userdata VALUES(?, ?, ?, ?) ON DUPLICATE KEY UPDATE username = VALUES(username), tag = VALUES(tag), avatar = VALUES(avatar)';
+  pool.query(queryString, [user.id, user.username, user.tag, user.displayAvatarURL])
 }
 
 function pickWheel() {
@@ -47,13 +52,13 @@ module.exports = {
   description: 'Spin a prize wheel once a day!',
   execute(message, args, client) {
     getLastSpin(message.member.id).then(function (results) {
-      var lastspin = results[0]
+      let lastspin = results[0]
       // If the user has previously spun the wheel, check that it's been 24 hours
       if (lastspin) {
         lastspin = results[0].lastspin
-        var current = new Date()
-        var elapsed = Math.abs(current - lastspin)
-        var delay = 12 * 3600 * 1000
+        const current = new Date()
+        const elapsed = Math.abs(current - lastspin)
+        const delay = 12 * 3600 * 1000
         if (elapsed < delay) {
           message.channel.send('Your next daily spin is in ' + client.timeToString(delay - elapsed))
           return
@@ -63,19 +68,20 @@ module.exports = {
       message.channel.send('Get ready!')
 
       // Spin the wheel!
-      var prizes = pickWheel()
+      const prizes = pickWheel()
       updateLastSpin(message.member.id, new Date())
+      updateUserData(message.member.user)
 
       client.executePython('spinner', prizes).then(function (data) {
-        var attachment = new discord.Attachment('./img/spinner.gif')
+        const attachment = new discord.Attachment('./img/spinner.gif')
         message.channel.send(attachment).then(function () {
           setTimeout(function () {
             // After a short delay, announce the winning and award credits
             if (data.startsWith('ZERO')) {
               message.channel.send('Oh no :( Better luck next time!')
             } else {
-              var amount = parseInt(data.replace('#coin ', ''), 10)
-              var coin = client.emojis.get('631834832300670976')
+              const amount = parseInt(data.replace('#coin ', ''), 10)
+              const coin = client.emojis.get('631834832300670976')
               message.channel.send(`Congrats! You won ${coin} ${amount}`)
               arcade.incrementArcadeCredits(message.member.id, amount)
             }
