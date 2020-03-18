c4eff0a05d1e021efcdbd9dc3f8854d8ac2ab8b9
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-03-18 12:27:15 +0800

    Roulette: Functionality

--- commands/roulette.js
+++ commands/roulette.js
@@ -11,8 +11,39 @@ bet_functions['odd'] = function(result) {
   return (result != 0 && result % 2 == 1)
 }
 
-function spinRoulette(message, betType, betAmount) {
+function spinRoulette(client, message, betType, betAmount) {
+  // Spin the wheel, brent!
+  const coin = client.emojis.get('631834832300670976')
 
+  client.executePython('roulette').then(function (result) {
+    result = parseInt(result) // just in case
+    const attachment = new discord.Attachment('./img/roulette.gif')
+    message.channel.send(attachment).then(function () {
+      setTimeout(function () {
+        // Check how the results went!
+        message.channel.send(`The wheel came up: ${result}`)
+        if(typeof betType == "string") {
+          // Function bet
+          if(bet_functions[betType](result)) {
+            message.channel.send(`Congratulations! You won ${coin} ${betAmount*2}`)
+            arcade.incrementArcadeCredits(message.member.id, betAmount*2)
+          } else {
+            message.channel.send('Better luck next time!')
+          }
+        } else {
+          // Single number bet
+          if(betType == result) {
+            message.channel.send(`Congratulations! You won ${coin} ${betAmount*36}`)
+            arcade.incrementArcadeCredits(message.member.id, betAmount*36)
+          } else {
+            message.channel.send('Better luck next time!')
+          }
+        }
+      }, 6500)
+    })
+  }).catch(function (err) {
+    message.channel.send(err.toString())
+  })
 }
 
 module.exports = {
@@ -21,13 +52,14 @@ module.exports = {
   execute(message, args, client) {
     // Determine the type of bet being made
     // Players can either pick a single number OR one of the above bet functions
-    const betType = args[0]
+    let betType = args[0]
     if(!bet_functions[betType]) {
       let betNumber = parseInt(args[0])
       if (isNaN(betNumber) || betNumber < 0 || betNumber > 36) {
         message.channel.send('Please enter a number between 0 and 36')
         return
       }
+      betType = betNumber
     }
 
     // Determine the amount of coins being bet
@@ -50,20 +82,24 @@ module.exports = {
       betAmount = amount
     }
 
-    // Confirm before spinning the wheel
-    message.channel.send(betType + ':' + betAmount)
-    return
+    // Send a confirmation message
+    message.channel.send(`Are you sure you want to bet ${betAmount} coins on ${betType}?`).then(function (msg) {
+      msg.react('✅')
+      const filter = function (reaction, user) {
+        return user.id === message.member.id && reaction.emoji.name === '✅'
+      }
+  
+      const collector = msg.createReactionCollector(filter, { time: 35000 })
+      collector.on('collect', function () {
+        // Confirmation received!
+        collector.stop()
+        msg.edit('Get ready!')
+        msg.clearReactions()
 
-
-    client.executePython('roulette').then(function (data) {
-      var attachment = new discord.Attachment('./img/roulette.gif')
-      message.channel.send(attachment).then(function () {
-        setTimeout(function () {
-          message.channel.send(data)
-        }, 6500)
+        // Spin the wheel
+        arcade.incrementArcadeCredits(message.member.id, -betAmount)
+        spinRoulette(client, message, betType, betAmount)
       })
-    }).catch(function (err) {
-      message.channel.send(err.toString())
     })
   }
 }
