6c4229512b81cb663380bb5aae7c80aacebdcd4b
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-03-26 14:28:54 +1000

    Advanced dice rolling

--- commands/dice.js
+++ commands/dice.js
@@ -1,7 +1,16 @@
 const discord = require('discord.js')
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+
+function testDiceRolling(max) {
+    let results = {}
+    for (let i = 0; i < 10000; i++) {
+        const result = getRandomInt(max)
+        results[result] = (result in results ? results[result] : 0) + 1
+    }
+    console.log(results)
 }
 
 module.exports = {
@@ -9,13 +18,43 @@ module.exports = {
     description: 'Roll a simple dice',
     aliases: ['roll'],
     execute(message, args) {
-        let max = 20
-        const maxGuess = parseInt(args[0])
-        if (!isNaN(maxGuess)) {
-            max = maxGuess
-        }
+        let rolls = args.map(arg => {
+            // Check if this is a multi-roll
+            let multiRoll = arg.match(/(?<number>\d+)d(?<max>\d+)/)
+            if (multiRoll) {
+                let number = parseInt(multiRoll.groups.number)
+                let max = parseInt(multiRoll.groups.max)
 
-        const result = getRandomInt(1, max)
-        message.channel.send(`🎲 ${result}`)
+                let total = 0
+                for (let i = 0; i < number; i++) {
+                    total += getRandomInt(max)
+                }
+
+                return total
+            } else {
+                // Check if this is a dN dice
+                let singleRoll = arg.match(/d(?<max>\d+)/)
+                if (singleRoll) {
+                    let max = parseInt(singleRoll.groups.max)
+                    return getRandomInt(max)
+                } else {
+                    // Check if this is a number
+                    try {
+                        let max = parseInt(arg)
+                        return getRandomInt(max)
+                    } catch (e) {
+                        return null
+                    }
+                }
+            }
+        })
+
+        // Send the result, excluding any invalid dice
+        rolls = rolls.filter(x => !!x)
+        if (rolls.length < 1) {
+            message.channel.send('Enter valid dice!')
+        } else {
+            message.channel.send(`🎲 ${rolls.join(" ")}`)
+        }
     }
 }
