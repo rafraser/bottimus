e656443e88471b18197ad11b70776db8ec446a8d
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-07-28 22:01:38 +1000

    Arcade + Database utilities

---
+++ src/database.ts
@@ -0,0 +1,9 @@
+import mysql from "mysql"
+
+export default mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'bottimus'
+})
