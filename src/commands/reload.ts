112d0ad438699b54e0d227bff61f9ac3259340b8
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-08-27 01:33:11 +1000

    Reload improvements
    - Now gives feedback
    - Multiple reload modes

--- src/commands/reload.ts
+++ src/commands/reload.ts
@@ -1,6 +1,13 @@
 import { Client, Message } from "../command"
 import { loadEvents } from "../events"
 
+const reload_modes = {
+    'settings': async (client) => client.loadServerSettings(),
+    'events': async (client) => loadEvents(client),
+    'commands': async (client) => client.loadCommands(),
+    'updaters': async (client) => client.loadUpdaters()
+} as { [key: string]: (client: Client) => Promise<any> }
+
 export default {
     name: 'reload',
     description: '🛡️ Reloads essential data',
@@ -12,7 +19,23 @@ export default {
             return
         }
 
-        client.loadServerSettings()
-        loadEvents(client)
+        if (args.length < 1) {
+            const modes = Object.keys(reload_modes).join(' ')
+            message.channel.send('Please select a reload mode:```all ' + modes + '```')
+            return
+        }
+
+        if (args[0] == 'all') {
+            for (let key in reload_modes) {
+                await reload_modes[key](client)
+                message.channel.send(`Reloaded ${key}`)
+            }
+        } else if (reload_modes[args[0]]) {
+            await reload_modes[args[0]](client)
+            message.channel.send(`Reloaded ${args[0]}`)
+        } else {
+            message.channel.send('Invalid reload mode.')
+            return
+        }
     }
 }
