d871b0e9b4da761cf49fbd5394b55ec4d70a8b5e
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-10-28 21:43:40 +1000

    Configurable database

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: 'bottimus',
+  database: process.env.DB_DATABASE || 'bottimus',
   timezone: 'UTC'
 })
 
