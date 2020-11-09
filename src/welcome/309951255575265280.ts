e2645beb75974d19271fea10d07be2cb0785088c
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-11-09 16:18:53 +1000

    Update Fluffy Servers settings

--- src/welcome/309951255575265280.ts
+++ src/welcome/309951255575265280.ts
@@ -3,5 +3,4 @@ import { GuildMember, TextChannel } from 'discord.js'
 export default (member: GuildMember) => {
   const chan = member.guild.channels.cache.find((ch) => ch.name === 'general') as TextChannel
   chan.send(`Welcome to Fluffy Servers, ${member.displayName}! Please check out <#528849382196379650>`)
-  member.roles.add('535346825423749120')
 }
