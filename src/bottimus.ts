cc4a25ea744304a2b812e247fc90ac9b50395a5d
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-12-21 22:41:13 +1000

    Update dotenv

--- src/bottimus.ts
+++ src/bottimus.ts
@@ -1,7 +1,13 @@
-import 'dotenv/config'
+/* eslint-disable import/first */
+// Load the .env file for our environment before any imports
+import dotenv from 'dotenv'
+dotenv.config({
+  path: `${process.env.BOTTIMUS_ENV || 'development'}.env`
+})
+
 import BottimusClient from './client'
 
-const client = new BottimusClient((process.env.ENV === 'development'), (process.env.PYTHON_PATH || 'python3'), {
+const client = new BottimusClient((process.env.BOTTIMUS_ENV === 'development'), (process.env.PYTHON_PATH || 'python3'), {
   ws: {
     intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_MEMBERS']
   }
