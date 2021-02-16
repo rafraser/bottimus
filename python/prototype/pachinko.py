9bc99c9669dfac5aa8c7a6684d6692773528b8e7
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-02-17 00:21:32 +1000

    Pachinko beta 2

--- python/prototype/pachinko.py
+++ python/prototype/pachinko.py
@@ -1,120 +1,118 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import math
 import random
 import itertools
+import physics
+
+DEFAULT_FONT40 = ImageFont.truetype("../../img/font/disco.ttf", 40)
+DEFAULT_FONT32 = ImageFont.truetype("../../img/font/disco.ttf", 32)
 
 
-class Vector(object):
-    def __init__(self, x=0, y=0):
-        self.x, self.y = x, y
+def generate_pegs(objects, width, height):
+    for (x, y) in itertools.product(
+        list(range(64, width, 64)), range(128, height, 128)
+    ):
+        objects.append(physics.Peg(x, y, 8))
 
-    def __add__(self, other):
-        if isinstance(other, Vector):
-            return Vector(self.x + other.x, self.y + other.y)
-        elif isinstance(other, tuple):
-            return Vector(self.x + other[0], self.y + other[0])
-
-    def __sub__(self, other):
-        return Vector(self.x - other.x, self.y - other.y)
-
-    def __mul__(self, other):
-        if isinstance(other, Vector):
-            return self.dot(other)
-        elif isinstance(other, (int, float)):
-            return Vector(self.x * other, self.y * other)
-
-    def dot(self, other):
-        return (self.x * other.x) + (self.y * other.y)
-
-    def magnitude(self):
-        return math.hypot(self.x, self.y)
-
-    def normalize(self):
-        norm = self.magnitude()
-        return Vector(self.x / norm, self.y / norm)
+    for (x, y) in itertools.product(
+        list(range(32, width + 64, 64)), range(192, height - 64, 128)
+    ):
+        objects.append(physics.Peg(x, y, 8))
 
 
-class Ball(object):
-    color = "#e84118"
-    bounciness = 0.65
+def generate_balls(objects, width):
+    # balls = []
+    # for x in range(32, width, 32):
+    #     for y in range(-60, 0, 24):
+    #         ball = physics.Ball(random.uniform(x - 8, x + 8), y)
+    #         ball.velocity = physics.Vector(
+    #             random.uniform(-1, 1), 1 + random.uniform(0, 1)
+    #         )
+    #         balls.append(ball)
+    # return balls
 
-    def __init__(self, x, y, r):
-        self.position = Vector(x, y)
-        self.velocity = Vector(0, 0)
-        self.radius = r
-
-    def tick(self):
-        self.velocity = self.velocity + GRAVITY
-        self.position = self.position + self.velocity
-
-        if self.position.x < 0:
-            self.position.x = 0
-            self.velocity = Vector(-self.velocity.x, self.velocity.y)
-        elif self.position.x > SIZE:
-            self.position.x = SIZE
-            self.velocity = Vector(-self.velocity.x, self.velocity.y)
-
-    def draw(self, draw):
-        x, y = self.position.x, self.position.y
-        r = self.radius
-        draw.ellipse((x - r, y - r, x + r, y + r), fill=self.color)
+    ball = physics.Ball(random.uniform(64, width - 64), -16)
+    ball.velocity = physics.Vector(random.uniform(-1, 1), 1 + random.uniform(0, 1))
+    return [ball]
 
 
-BG_COLOR = "#353b48"
-BALL_COLOR = "#e84118"
-PEG_COLOR = "#718093"
-GRAVITY = Vector(0, 0.5)
-SIZE = 512
+def generate_blocks(objects, width, height):
+    objects.append(physics.Block(0, -64, 8, height))
+    objects.append(physics.Block(width - 8, -64, width, height))
+    objects.append(physics.Block(0, -68, width, -64))
+
+    for x in range(128, width, 128):
+        objects.append(physics.Block(x - 4, height - 32, x + 4, height))
 
 
-def simulation_step(ball, pegs):
-    ball.tick()
-
-    # Collision check
-    for peg in pegs:
-        pegpos = Vector(peg[0], peg[1])
-        diff = ball.position - pegpos
-        distance = diff.magnitude()
-
-        if distance <= ball.radius + peg[2]:
-            normal = (ball.position - pegpos).normalize()
-            reflection = ball.velocity - normal * (ball.velocity.dot(normal) * 2)
-            ball.velocity = reflection * ball.bounciness
-            ball.position = ball.position + diff * 0.5
+def render_background(width, height):
+    return Image.new("RGBA", (width, height), color="#353b48")
 
 
-def render_peg(draw, peg):
-    x = peg[0]
-    y = peg[1]
-    r = peg[2]
-    draw.ellipse((x - r, y - r, x + r, y + r), fill=PEG_COLOR)
+def centered_text(draw, x, y, text, font, fill="white"):
+    tw, th = draw.textsize(text, font=font)
+    draw.text((x - (tw / 2), y - (th / 2)), text, font=font, fill=fill)
 
 
-def render_scene(draw, ball, pegs):
-    ball.draw(draw)
+def render_overlay(width, height):
+    canvas = Image.new("RGBA", (width, height))
+    draw = ImageDraw.Draw(canvas)
 
-    for peg in pegs:
-        render_peg(draw, peg)
+    # Pachinko Header
+    font = DEFAULT_FONT40
+    draw.rectangle((8, 0, width - 8, 48), fill="#70a1ff")
+    centered_text(draw, width / 2, 24, "P  A  C  H  I  N  K  O", font)
+
+    # Bins
+    font = DEFAULT_FONT32
+    draw.rectangle((8, height - 28, 124, height), fill=(232, 67, 147))
+    centered_text(draw, 66, height - 14, "100", font)
+    draw.rectangle((132, height - 28, 252, height), fill=(108, 92, 231))
+    centered_text(draw, 192, height - 14, "1000", font)
+    draw.rectangle((260, height - 28, width - 8, height), fill=(0, 206, 201))
+    centered_text(draw, 322, height - 14, "200", font)
+    return canvas
+
+
+def all_balls_gone(balls, height):
+    return all([ball.position.y > height for ball in balls])
 
 
 def build_gif():
+    BG_COLOR = "#2f3640"
+    HEIGHT = 512
+    WIDTH = 384
     frames = []
-    pegs = [
-        (x, y, 4)
-        for (x, y) in itertools.product(list(range(64, 512, 64)), range(128, 512, 128))
-    ] + [
-        (x - 32, y, 4)
-        for (x, y) in itertools.product(list(range(64, 576, 64)), range(192, 512, 128))
-    ]
 
-    ball = Ball(48 + random.uniform(-10, 10), 10, 16)
-    for i in range(240):
-        frame = Image.new("RGBA", (512, 512), color=BG_COLOR)
+    # Build the physics scene
+    objects = []
+    generate_pegs(objects, WIDTH, HEIGHT)
+    generate_blocks(objects, WIDTH, HEIGHT)
+    balls = generate_balls(objects, WIDTH)
+    objects = objects + balls
+
+    scene = physics.Scene(objects, gravity=physics.Vector(0, 0.25))
+
+    # Setup the renderer
+    background = render_background(WIDTH, HEIGHT)
+    overlay = render_overlay(WIDTH, HEIGHT)
+
+    physics_calcs_per_frame = 2
+    step_number = 0
+    while step_number < 2000 and not all_balls_gone(balls, HEIGHT):
+        frame = background.copy()
         draw = ImageDraw.Draw(frame)
-        simulation_step(ball, pegs)
+        scene.step(delta=0.4)
+        step_number += 1
 
-        render_scene(draw, ball, pegs)
-        frames.append(frame)
+        if step_number % physics_calcs_per_frame == 0:
+            scene.draw(draw)
+            frame.alpha_composite(overlay)
+            frames.append(frame)
+
+    # Hang on before restarting
+    durations = [20] * len(frames)
+    durations[-1] = 1000
 
     # Save the gif to a file
     frames[0].save(
@@ -122,7 +120,7 @@ def build_gif():
         format="GIF",
         append_images=frames[1:],
         save_all=True,
-        duration=20,
+        duration=durations,
         loop=0,
     )
 
