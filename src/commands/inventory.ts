a80e3fd7b786e6bebc6185a79bf3308f6fd7db4f
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2022-01-25 21:23:37 +1000

    Fix crashes with inventory command

--- src/commands/inventory.ts
+++ src/commands/inventory.ts
@@ -9,22 +9,25 @@ export default {
   cooldown: 15,
 
   async execute (client: Client, message: Message, args: string[]) {
-    try {
-      const user = await client.findUser(message, args, true)
-      const prizes = await getArcadePrizes(user.id)
+    const user = await client.findUser(message, args, true)
+    const prizes = await getArcadePrizes(user.id)
 
-      const pythonArgs = ['--prizes']
-      for (const prize in prizes) {
-        pythonArgs.push(prize + ':' + prizes[prize])
-      }
-
-      await client.executePython('inventory', pythonArgs)
-      const attachment = new MessageAttachment('./img/inventory.png')
-      message.channel.send({ files: [attachment] })
-
-      client.updateCooldown(this, message.member.id)
-    } catch (e) {
-      message.channel.send(e.message)
+    // Guard for empty inventories
+    if (prizes.length < 1) {
+      message.channel.send(`${user.displayName} has collected no prizes!`)
+      return
     }
+
+    // Generate an inventory image and send it
+    const pythonArgs = ['--prizes']
+    for (const prize in prizes) {
+      pythonArgs.push(prize + ':' + prizes[prize])
+    }
+
+    await client.executePython('inventory', pythonArgs)
+    const attachment = new MessageAttachment('./img/inventory.png')
+    message.channel.send({ files: [attachment] })
+
+    client.updateCooldown(this, message.member.id)
   }
 }
